14d7fa214f8ed58983c40bcf401d04e204cbb33d
hosseinGith <hosseinderee7@gmail.com>
2024-12-13 19:50:59 +0330

    add all

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  safelist: ["flex ", "flex-col", "py-4"],
+  theme: {
+    colors: {
+      success: "green",
+      reject: "red",
+      mainColorBg: "#0277f8",
+      mainColorText: "#fff",
+      secendColor: "#f2f2f2",
+      tableColor: "#f2f2f2",
+      borderColor: "#dddee1",
+      bodyColor: "#f5f6fa",
+      fontColor: "#777777",
+    },
+  },
+  plugins: [],
+};
